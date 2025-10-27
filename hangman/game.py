e1ed5a3dfb97312e825a333a0a5d1976e41fcd61
Haim Aflalo <kodkodhaim@gmail.com>
2025-10-27 11:05:57 +0200

    adding is_won,is_lost,render_display,render_summary functions

--- hangman/game.py
+++ hangman/game.py
@@ -1,12 +1,12 @@
 from hangman.words import choose_secret_words
 
 
-def init_state(secret , max_tries = 10):
+def init_state(secret , max_tries):
     lst = ["_"]*len(secret)
-    game_dict = {"s_word":secret,
+    game_dict = {"secret":secret,
                  "display":lst,
                  "guessed":{},
-                 "wrong_guesses":None,
+                 "wrong_guesses":0,
                  "max_tries":max_tries}
     return game_dict
 
@@ -14,6 +14,17 @@ def init_state(secret , max_tries = 10):
 def validate_guess(ch,guessed):
     return ch, ch not in guessed
 
+def is_won(state):
+    return "_" not in state["display"]
+
+def is_lost(state):
+    return state["max_tries"] >= state["wrong_guesses"]
+
+def render_display(state: dict):
+    return state["display"]
+
+def render_summary(state: dict):
+    return state["secret"],state["guessed"]
 
 
 
