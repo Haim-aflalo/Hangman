f221a9973749372b9fe5a6accdc5de26655002e9
Haim Aflalo <kodkodhaim@gmail.com>
2025-10-27 10:15:50 +0200

    updating the main and adding data folder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from hangman.words import choose_secret_words
-from hangman.words import words
+from data import lst_words
+lst = lst_words.words
+
 if __name__ == "__main__":
-    print(choose_secret_words(words))
+    print(choose_secret_words(lst))
